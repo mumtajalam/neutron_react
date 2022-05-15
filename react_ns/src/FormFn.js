72f7abd78feaf74c8a08474fb2fe1b2df0746503
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-05-16 00:51:27 +0530

    react

--- react_ns/src/FormFn.js
+++ react_ns/src/FormFn.js
@@ -14,26 +14,43 @@ const FormFn = () => {
         console.log('Form Data', username, email, password);
         setFormStatus(true);
         // Api call
-        
-
+        setUsername('');
+        setEmail('');
+        setPassward('');
+    }
+    const input_username = (event) => {
+        console.log('input username', event.target.value);
+        setUsername(event.target.value);
+    }
+    const input_email = (event) => {
+        console.log('input email', event.target.value);
+        setEmail(event.target.value);
+    }
+    const input_password = (event) => {
+        console.log('input password', event.target.value);
+        setPassward(event.target.value);
     }
     
     return(
         <>
-           <br></br>
-            <div><h2>Registration Form</h2></div>
             <br></br>
-            Name:
-            <input type="text" placeholder=""/>
-            <br></br>
-            Email:
-            <input type="email" placeholder=""/>
-            <br></br>
-            password:
-            <input type="password" placeholder=""/>
-            <br></br>
-            <button>Register</button>
+                <form>
+                    <div><h2>Registration Form</h2></div>
+                    <br></br>
+                    Name:
+                    <input type="text" name="username" onChange={input_username} value={username}/>
+                    <br></br>
+                    Email:
+                    <input type="email" name="email" onChange={input_email} value={email}/>
+                    <br></br>
+                    password:
+                    <input type="password" name="password" onChange={input_password} value={password}/>
+                    <br></br>
+                    <button type="submit" onClick={handleSubmit}>Register</button>
+                </form>
             <br></br> 
+            {formStatus &&<div>Form successfully Submitted...</div>}
+            <br></br>
         </>
     )
 }
